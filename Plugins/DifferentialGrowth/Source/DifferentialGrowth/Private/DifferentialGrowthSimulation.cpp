5523d0b16088d4e49715213a655bcede3c9e2fbe
tdhooper <projects@stainlessvision.com>
2025-03-14 15:32:12 +0000

    Create DifferentialGrowth plugin

---
+++ Plugins/DifferentialGrowth/Source/DifferentialGrowth/Private/DifferentialGrowthSimulation.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DifferentialGrowthSimulation.h"
+
+// Sets default values
+ADifferentialGrowthSimulation::ADifferentialGrowthSimulation()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADifferentialGrowthSimulation::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADifferentialGrowthSimulation::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
